53d13e6ae50803959e8be5a6ad242dfeecb872fb
Taha Mostafa <20231700229@cis.asu.edu.eg>
2025-05-07 02:06:58 +0300

    Fixed some functions
    Added encryption with modular exponentation.

--- BigInteger/BigInt.cs
+++ BigInteger/BigInt.cs
@@ -1,12 +1,12 @@
 ﻿
 using System;
 using System.Collections.Generic;
-namespace bigInteger 
+namespace bigInteger
 {
     public class BigInt
     {
         public LinkedList<int> arr;
-
+        private static Random rand = new Random();
 
         public BigInt(string val)
         {
@@ -24,22 +24,10 @@ namespace bigInteger
             arr = new LinkedList<int>();
         }
 
-        public static bool isEven(BigInt n)
+        public bool isEven()
         {
 
-            LinkedListNode<int> lastN = n.arr.Last;
-            try
-            {
-                if (lastN == null)
-                {
-                    return false;
-                }
-                return (lastN.Value % 2 == 0);
-            }
-            catch (Exception)
-            {
-                return false;
-            }
+            return (this.arr.Last != null && this.arr.Last.Value % 2 == 0);
         }
 
         public static void RemoveFrontZeros(ref BigInt n)
@@ -58,7 +46,7 @@ namespace bigInteger
             {
                 return new BigInt("1");
             }
-            if (isEven(n))
+            if (n.isEven())
             {
                 BigInt half = Power(x, divide(n, new BigInt("2")).Quotient);
                 RemoveFrontZeros(ref half);
@@ -110,8 +98,8 @@ namespace bigInteger
             BigInt a_plus_b_Mult_c_plus_d = Multiplication(a_plus_b, c_plus_d);
             BigInt ad_Plus_bc = subtract(subtract(a_plus_b_Mult_c_plus_d, ac), bd);
 
-            BigInt result1 = AddZerosLast(ref ac, (n / 2) * 2); // shift ac by 2 * (n/2)
-            BigInt result2 = AddZerosLast(ref ad_Plus_bc, n / 2); // shift ad+bc by (n/2)
+            BigInt result1 = AddZerosLast(ref ac, (n / 2) * 2);
+            BigInt result2 = AddZerosLast(ref ad_Plus_bc, n / 2);
 
 
             BigInt finalResult = sum(result1, result2);
@@ -179,135 +167,115 @@ namespace bigInteger
             }
             return sum;
         }
- public static (BigInt result, BigInt remain) divide(BigInt num, BigInt divisor)
- {
-     if (divisor.ToString() == "0")
-     {
-         throw new ArithmeticException("Can't divide by zero");
-     }
-     if (compare(num, divisor) == 0)
-     {
-         return (new BigInt("1"), new BigInt("0"));
-     }
-     else if (compare(num, divisor) == -1)
-     {
-         return (new BigInt("0"), new BigInt(divisor.ToString()));
-     }
-     else
-     {
-         BigInt result = new BigInt();
-         BigInt remain = new BigInt("0");
-         LinkedListNode<int> currentDigit = num.arr.First;
-         while (currentDigit != null)
-         {
-             remain.arr.AddLast(currentDigit.Value);
-             if (compare(remain, divisor) == -1)
-             {
-                 result.arr.AddLast(0);
-                 currentDigit = currentDigit.Next;
-                 continue;
-             }
-             int divideCount = 0;
-             while (compare(remain, divisor) >= 0)
-             {
-                 remain = subtract(remain, divisor);
-                 divideCount++;
-
-             }
-             result.arr.AddLast(divideCount);
-             currentDigit = currentDigit.Next;
-         }
-         return (result, remain);
-
-     }
- 
-
-    
- }
-         public static int compare(BigInt a, BigInt b)
-     {
-         removeLeadingZeroes(a);
-         removeLeadingZeroes(b);
-
-         if (a.arr.Count > b.arr.Count)
-          return 1;
-
-         else if (a.arr.Count < b.arr.Count) return -1;
-         else
-         {
-
-             LinkedListNode<int> n1= a.arr.First;
-             LinkedListNode<int> n2 = b.arr.First;
-             while (n1 != null || n2!=null)
-             {
-                 if (n1.Value > n2.Value) return 1;
-                 if (n1.Value < n2.Value) return -1;
-                 n1 = n1.Next;
-                 n2 = n2.Next;
-             }
-             return 0;
-
-         }
-     }
-public static BigInt subtract(BigInt x, BigInt y) {
-      LinkedListNode<int> num1 = x.arr.Last;
-      LinkedListNode<int> num2 = y.arr.Last;
-      BigInt result= new BigInt();
-      int borrow = 0;
-      while(num1 != null || num2!=null) 
-      {
-          int a, b;
-          if (num1!=null)
-          {
-              a=num1.Value;
-          }
-          else
-          {
-              a = 0;
-          }
-          if (num2 != null)
-          {
-              b = num2.Value;
-          }
-          else
-          {
-              b = 0;
-          }
-          int difference = a - b - borrow;
-          if (difference < 0)
-          {
-              difference = difference + 10;
-              borrow = 1;
-          }
-          else
-          {
-              borrow= 0;
-          }
-           result.arr.AddFirst(difference);
-          if (num1!=null)
-          {
-              num1 = num1.Previous;
-          }
-          if (num2 != null)
-          {
-              num2 = num2.Previous;
-          }
-
-
-      }
-
-      return result;
-  
- }
-        
-        
-        public static void removeLeadingZeroes(BigInt num)
+        public static (BigInt Quotient, BigInt Remainder) divide(BigInt num, BigInt divisor)
         {
-            while (num.arr.First.Value==0)
+            if (compare(num, divisor) == -1)
+                return (new BigInt("0"), num);
+
+            else
             {
-                num.arr.RemoveFirst();
+                BigInt quotient = new BigInt();
+                BigInt remainder = new BigInt();
+                (quotient, remainder) = divide(num, sum(divisor, divisor));
+                quotient = sum(quotient, quotient);
+                if (compare(remainder, divisor) == -1)
+                {
+                    return (quotient, remainder);
+                }
+                else
+                {
+                    return (sum(quotient, new BigInt("1")), subtract(remainder, divisor));
+                }
             }
-          
+
+
+
+        }
+        public static int compare(BigInt a, BigInt b)
+        {
+
+            if (a.arr.Count > b.arr.Count)
+                return 1;
+
+            else if (a.arr.Count < b.arr.Count) return -1;
+            else
+            {
+
+                LinkedListNode<int> n1 = a.arr.First;
+                LinkedListNode<int> n2 = b.arr.First;
+                while (n1 != null || n2 != null)
+                {
+                    if (n1.Value > n2.Value) return 1;
+                    if (n1.Value < n2.Value) return -1;
+                    n1 = n1.Next;
+                    n2 = n2.Next;
+                }
+                return 0;
+
+            }
+        }
+        public static BigInt subtract(BigInt x, BigInt y)
+        {
+
+            if (compare(x, y) == 0) return new BigInt("0");
+
+
+            LinkedListNode<int> num1 = x.arr.Last;
+            LinkedListNode<int> num2 = y.arr.Last;
+            BigInt result = new BigInt();
+            int borrow = 0;
+            while (num1 != null || num2 != null)
+            {
+                int a, b;
+                if (num1 != null)
+                {
+                    a = num1.Value;
+                }
+                else
+                {
+                    a = 0;
+                }
+                if (num2 != null)
+                {
+                    b = num2.Value;
+                }
+                else
+                {
+                    b = 0;
+                }
+                int difference = a - b - borrow;
+                if (difference < 0)
+                {
+                    difference = difference + 10;
+                    borrow = 1;
+                }
+                else
+                {
+                    borrow = 0;
+                }
+                result.arr.AddFirst(difference);
+                if (num1 != null)
+                {
+                    num1 = num1.Previous;
+                }
+                if (num2 != null)
+                {
+                    num2 = num2.Previous;
+                }
+
+
+            }
+            return result.removeLeadingZeroes();
+        }
+
+
+        public BigInt removeLeadingZeroes()
+        {
+            while (this.arr.First != null && this.arr.First.Value == 0)
+            {
+                this.arr.RemoveFirst();
+            }
+            return this;
         }
 
         override public String ToString()
@@ -339,14 +307,33 @@ public static BigInt subtract(BigInt x, BigInt y) {
 
         public static BigInt encrypt(BigInt num, BigInt key, BigInt mod)
         {
-            return divide(Power(num, key), mod).Remainder;
+            if (num.arr.Last.Value == 0) 
+            {
+                return new BigInt("0");
+            }
+
+            if(key.arr.Last.Value == 0 )
+            {
+                return new BigInt("1");
+            }
+            BigInt result = new BigInt();
+            if (key.isEven())
+            {
+                result = encrypt(num, divide(key, new BigInt("2")).Quotient, mod);
+                result = divide(Multiplication(result, result), mod).Remainder;
+            }
+            else
+            {
+                result = divide(num, mod).Remainder;
+                BigInt middleValue = encrypt(num, subtract(key, new BigInt("1")), mod);
+                middleValue = Multiplication(result, middleValue);
+                middleValue = divide(middleValue, mod).Remainder;
+                result = divide(middleValue, mod).Remainder;
+            }
+            return (divide(sum(result, mod), mod).Remainder);
         }
 
-        public static BigInt decrypt(BigInt fnum, BigInt key, BigInt mod)
-        {
-            return divide(Power(fnum, key), mod).Remainder;
-        }
-
+     
         public static bool operator <=(BigInt left, BigInt right)
         {
             if (compare(left, right) <= 0)
@@ -383,86 +370,26 @@ public static BigInt subtract(BigInt x, BigInt y) {
             return false;
         }
 
-        public static BigInt ModulusFactor(BigInt n)
-        {
-            BigInt p;
-            BigInt q;
-            BigInt phi;
-            for (BigInt i = new BigInt("2"); Multiplication(i, i) <= n; i = sum(i, new BigInt("1")))
-            {
-                if (divide(n, i).Remainder.ToString() == "0") // Changed Equals(0) to ToString() == "0"
-                {
-                    q = i;
-                    p = divide(n, i).Quotient;
-                    phi = Multiplication(subtract(q, new BigInt("1")), subtract(p, new BigInt("1")));
-                    RemoveFrontZeros(ref phi);
-                    return phi;
-                }
-            }
-            return new BigInt("1");
-        }
+        //public static BigInt ModulusFactor(BigInt n)
+        //{
+        //    BigInt p;
+        //    BigInt q;
+        //    BigInt phi;
+        //    for (BigInt i = new BigInt("2"); Multiplication(i, i) <= n; i = sum(i, new BigInt("1")))
+        //    {
+        //        if (divide(n, i).Remainder.arr.Last.Value == 0)
+        //        {
+        //            q = i;
+        //            p = divide(n, i).Quotient;
+        //            phi = Multiplication(subtract(q, new BigInt("1")), subtract(p, new BigInt("1")));
+        //            RemoveFrontZeros(ref phi);
+        //            return phi;
+        //        }
+        //    }
+        //    return new BigInt("1");
+        //}
 
-        public static (BigInt gcd, BigInt x, BigInt y) ExtendedEuclidean(BigInt a, BigInt b)
-        {
-            if (a.ToString() == "0") // Changed a.Equals(0) to a.ToString() == "0"
-            {
-                return (b, new BigInt("0"), new BigInt("1"));
-            }
-            else
-            {
-                var (gcd, x1, y1) = ExtendedEuclidean(divide(b, a).Remainder, a);
-                var division = divide(b, a);
-                BigInt x = y1;
-                BigInt y = subtract(x1, Multiplication(division.Quotient, y1));
-                return (gcd, x, y);
-            }
-        }
-
-        public static BigInt ModInverse(BigInt a, BigInt m)
-        {
-            var (gcd, x, y) = ExtendedEuclidean(a, m);
-            if (!gcd.ToString().Equals("1")) // Changed gcd != new BigInt("1") to !gcd.ToString().Equals("1")
-            {
-                throw new Exception("Modular inverse doesn't exist");
-            }
-            else
-            {
-                // Ensure the result is positive
-                BigInt result = divide(x, m).Remainder;
-                if (compare(result, new BigInt("0")) < 0) // Changed result < new BigInt("0") to compare(result, new BigInt("0")) < 0
-                {
-                    result = sum(result, m);
-                }
-                return result;
-            }
-        }
-
-        public static BigInt GeneratePrivateKey(BigInt phi, BigInt e)
-        {
-            // The private key d is the modular inverse of e modulo φ(n)
-            return ModInverse(e, phi);
-        }
-
-        private static Random rand = new Random();
-
-        public static (BigInt n, BigInt e, BigInt d) GeneratePublicKey(int digitSize )
-        {
-            BigInt p = generateprime(digitSize);
-            BigInt q = generateprime(digitSize);
-            while (BigInt.Equals(p,q)) 
-            {
-                q = generateprime(digitSize);
-            }
-
-            BigInt n = Multiplication(p, q);
-            BigInt phi = Multiplication(subtract(p, new BigInt("1")), subtract(q, new BigInt("1")));
-            BigInt e = SelectPublicExponent(phi);
-            BigInt d = ModInverse(e, phi);
-            
-            return (n, e, d);
-        }
-
-        private static BigInt generateprime(int digits)
+        public static BigInt generateprime(int digits)
         {
             // Define the range for the prime number
             BigInt lowerBound = lowerbound(digits);
@@ -474,7 +401,7 @@ public static BigInt subtract(BigInt x, BigInt y) {
                 BigInt candidate = GenerateRandomBigInt(lowerBound, upperBound);
 
                 // Make sure it's odd
-                if (BigInt.isEven(candidate))
+                if (candidate.isEven())
                     candidate = BigInt.sum(candidate, new BigInt("1"));
 
                 // Perform primality test
@@ -485,19 +412,16 @@ public static BigInt subtract(BigInt x, BigInt y) {
 
         private static BigInt GenerateRandomBigInt(BigInt min, BigInt max)
         {
-            // Get the number of digits in max
+        
             string maxStr = max.ToString();
             int maxDigits = maxStr.Length;
-
-            // Generate a random number with appropriate number of digits
+         
             while (true)
             {
                 string randomValue = "";
 
-                // First digit shouldn't be 0
-                randomValue += (char)('1' + rand.Next(9));
+                randomValue += (char)('0' + rand.Next(1,10));
 
-                // Generate remaining digits
                 for (int i = 1; i < maxDigits; i++)
                 {
                     randomValue += (char)('0' + rand.Next(10));
@@ -505,11 +429,28 @@ public static BigInt subtract(BigInt x, BigInt y) {
 
                 BigInt result = new BigInt(randomValue);
 
-                // Check if in range
-                if (BigInt.compare(result, min) >= 0 && BigInt.compare(result, max) <= 0)
+                if (compare(result, min) >= 0 && compare(result, max) <= 0)
                     return result;
             }
         }
+
+        public static (BigInt n, BigInt e) GeneratePublicKey(int digitSize)
+        {
+            BigInt p = generateprime(digitSize);
+            BigInt q = generateprime(digitSize);
+            while (BigInt.Equals(p, q))
+            {
+                q = generateprime(digitSize);
+            }
+
+            BigInt n = Multiplication(p, q);
+            BigInt phi = Multiplication(subtract(p, new BigInt("1")), subtract(q, new BigInt("1")));
+            BigInt e = SelectPublicExponent(phi);
+    //        BigInt d = ModInverse(e, phi);
+
+            return (n, e);
+        }
+
 
         private static BigInt lowerbound(int digits)
         {
@@ -530,13 +471,13 @@ public static BigInt subtract(BigInt x, BigInt y) {
         static bool IsPrime(BigInt number)
         {
             if (compare(number, new BigInt("2")) < 0) return false;
-            if (BigInt.Equals(number, new BigInt("2")) || BigInt.Equals(number, new BigInt("3"))) return true; 
-            if (BigInt.isEven(number)) return false;
+            if (BigInt.Equals(number, new BigInt("2")) || BigInt.Equals(number, new BigInt("3"))) return true;
+            if (number.isEven()) return false;
 
-            
+
             for (BigInt i = new BigInt("3"); Multiplication(i, i) <= number; i = sum(i, new BigInt("2")))
             {
-                if (BigInt.Equals( divide(number, i).Remainder,new BigInt( "0"))) 
+                if (divide(number, i).Remainder.arr.Last.Value == 0)
                     return false;
             }
 
--- BigInteger/Program.cs
+++ BigInteger/Program.cs
@@ -7,181 +7,20 @@ namespace bigInteger
     {
         static void Main(string[] args)
         {
-            // Performance testing setup
-            Stopwatch stopwatch = new Stopwatch();
-            Console.WriteLine("hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh");
-            Console.WriteLine(BigInt.divide(new BigInt("2"), new BigInt("22")).Remainder);
-            Console.WriteLine(BigInt.divide(new BigInt("22"), new BigInt("2")).Remainder);
 
-            //  Console.WriteLine(BigInt.decrypt(new BigInt("746"), new BigInt("2563"), new BigInt("3713")));
-            Console.WriteLine("==== Public Key Generation &&Key Generation  ===");
-            Console.WriteLine("(N, e,d)");
-            Console.WriteLine(  BigInt.GeneratePublicKey());
+            int before = System.Environment.TickCount;
+            Console.WriteLine("Before run: " + before);
 
-            // ========== ADDITION TEST CASES ==========
-            Console.WriteLine("=== ADDITION TESTS ===");
-            TestAddition("0", "0", "0");
-            TestAddition("5", "7", "12");
-            TestAddition("123", "456", "579");
-            TestAddition("999", "1", "1000");
-            TestAddition("57329817389217483219843341245621641542354235423543254325",
-                        "55435432543729845789275437297438547543254325432543254344",
-                        "112765249932947329009118778543060189085608560856086508669");
+            BigInt Prime = new BigInt();
+            Prime = BigInt.generateprime(1000);
 
-            // ========== SUBTRACTION TEST CASES ==========
-            Console.WriteLine("\n=== SUBTRACTION TESTS ===");
-            TestSubtraction("5", "3", "2");
-            TestSubtraction("100", "1", "99");
-            TestSubtraction("1234", "567", "667");
-            TestSubtraction("1000", "999", "1");
-            TestSubtraction("57329817389217483219843341245621641542354235423543254325",
-                           "55435432543729845789275437297438547543254325432543254344",
-                           "1894384845487637430567903948183093999100009990999999981");
+            Console.WriteLine(Prime.ToString());
 
-            // ========== MULTIPLICATION TEST CASES ==========
-            Console.WriteLine("\n=== MULTIPLICATION TESTS ===");
-            TestMultiplication("10", "100", "1000");
-            TestMultiplication("1234", "5678", "7006652");  // Your original test case
-            TestMultiplication("123", "456", "56088");
-            TestMultiplication("999", "999", "998001");
-            TestMultiplication("123456789", "987654321", "121932631112635269");
-            TestMultiplication("57329817389217483219843341245621641542354235423543254325",
-                             "55435432543729845789275437297438547543254325432543254344",
-                             "3178767850521823213438162548979797456012759470499004341405669092380931914349760613990090250240481300");
+            int after = System.Environment.TickCount;
+            Console.WriteLine("After run: " + after);
 
-            // ========== DIVISION TEST CASES ==========
-            Console.WriteLine("\n=== DIVISION TESTS ===");
-            TestDivision("10", "2", "5");
-            TestDivision("100", "25", "4");
-            TestDivision("123456", "789", "156");
-            TestDivision("999999", "111111", "9");
-            TestDivision("57329817389217483219843341245621641542354235423543254325",
-                       "123456789",
-                       "464388341827717642859347298572475814880");
+            Console.WriteLine("Difference in ticks: " + (after - before));
 
-            // ========== PERFORMANCE TESTING ==========
-            Console.WriteLine("\n=== PERFORMANCE TESTS ===");
-            PerformanceTest("12345678901234567890", "98765432109876543210");
-            PerformanceTest("57329817389217483219843341245621641542354235423543254325",
-                          "55435432543729845789275437297438547543254325432543254344");
-
-
-
-
-            // ========== POWER TEST CASES ==========
-            Console.WriteLine("\n=== POWER TESTS ===");
-            TestPower("2", "3", "8");
-            TestPower("5", "0", "1");       // anything^0 = 1
-            TestPower("7", "1", "7");
-            TestPower("10", "2", "100");
-            TestPower("12", "3", "1728");
-            TestPower("123", "2", "15129");
-            TestPower("999", "3", "997002999");
-            TestPower("123456789", "2", "15241578750190521");
-            TestPower("3", "10", "59049");
-            TestPower("57329817389217483219843341245621641542354235423543254325", "1",
-                      "57329817389217483219843341245621641542354235423543254325"); // power of 1 (identity)
-
-
-
-        }
-
-        static void TestAddition(string num1, string num2, string expected)
-        {
-            TestOperation(num1, num2, expected, (a, b) => BigInt.sum(a, b), "+");
-        }
-
-        static void TestSubtraction(string num1, string num2, string expected)
-        {
-            TestOperation(num1, num2, expected, (a, b) => BigInt.subtract(a, b), "-");
-        }
-
-        static void TestMultiplication(string num1, string num2, string expected)
-        {
-            TestOperation(num1, num2, expected, (a, b) => BigInt.Multiplication(a, b), "×");
-        }
-
-        static void TestDivision(string num1, string num2, string expected)
-        {
-            TestOperation(num1, num2, expected, (a, b) => BigInt.divide(a, b).Quotient, "÷");
-        }
-        // Add this function after TestDivision
-        static void TestPower(string baseNum, string exponentNum, string expected)
-        {
-            TestOperation(baseNum, exponentNum, expected, (a, b) => BigInt.Power(a, b), "^");
-        }
-
-
-
-        static void TestOperation(string num1, string num2, string expected,
-                                Func<BigInt, BigInt, BigInt> operation, string opSymbol)
-        {
-            try
-            {
-                BigInt a = new BigInt(num1);
-                BigInt b = new BigInt(num2);
-                BigInt result = operation(a, b);
-                string actual = result.ToString();
-
-                if (actual == expected)
-                {
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine($"PASS: {num1} {opSymbol} {num2} = {actual}");
-                }
-                else
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine($"FAIL: {num1} {opSymbol} {num2}");
-                    Console.WriteLine($"  Expected: {expected}");
-                    Console.WriteLine($"  Actual:   {actual}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine($"ERROR: {num1} {opSymbol} {num2}");
-                Console.WriteLine($"  Exception: {ex.Message}");
-            }
-            finally
-            {
-                Console.ResetColor();
-            }
-        }
-
-        static void PerformanceTest(string num1, string num2)
-        {
-            Console.WriteLine($"\nPerformance Test: {num1.Substring(0, Math.Min(10, num1.Length))}... × {num2.Substring(0, Math.Min(10, num2.Length))}...");
-
-            BigInt a = new BigInt(num1);
-            BigInt b = new BigInt(num2);
-
-            Stopwatch sw = new Stopwatch();
-
-            // Warm up
-            BigInt.sum(a, b);
-
-            // Addition test
-            sw.Start();
-            BigInt sum = BigInt.sum(a, b);
-            sw.Stop();
-            Console.WriteLine($"Addition: {sw.ElapsedMilliseconds} ms");
-            sw.Reset();
-
-            // Multiplication test
-            sw.Start();
-            BigInt product = BigInt.Multiplication(a, b);
-            sw.Stop();
-            Console.WriteLine($"Multiplication: {sw.ElapsedMilliseconds} ms");
-            Console.WriteLine($"Result length: {product.arr.Count} digits");
-
-
-            // inside PerformanceTest
-            // Power test
-            sw.Start();
-            BigInt powResult = BigInt.Power(a, new BigInt("5"));  // moderate exponent, not too big
-            sw.Stop();
-            Console.WriteLine($"Power (exp=5): {sw.ElapsedMilliseconds} ms");
-            Console.WriteLine($"Power Result length: {powResult.arr.Count} digits");
 
         }
     }
